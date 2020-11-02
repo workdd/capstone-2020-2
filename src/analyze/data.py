9c9e79daed77aa77d815eb3d38c408c546fb116c
JinseonHeo <wlstjs2478@naver.com>
2020-11-02 19:05:12 +0900

    Refactor #8 - 하이라이트 추출 함수에 삼항연산자 적용, 반복문 개선

--- src/analyze/data.py
+++ src/analyze/data.py
@@ -4,10 +4,10 @@ class Data:
     def __init__(self, platform, video_id):
         self.platform = platform
         self.video_id = video_id
-        self.data = None # 단위 시간당 볼륨, 채팅량 카운드
+        self.data = [] # 단위 시간당 볼륨, 채팅량 카운드
         self.unit_of_time = 30
-        self.point = None #하이라이트 지점
-        self.section = None
+        self.point = [] #하이라이트 지점
+        self.section = []
 
     def analyze_highlight(self):
         count = []
@@ -17,28 +17,21 @@ class Data:
         count.sort(key=lambda ele: ele[1], reverse=True)
         self.point = count[0:10]
 
-        i = 0
-        while i < len(self.point) - 1:
+        for i in range(len(self.point)-1):
             j = i + 1
             while j < len(self.point):
                 if abs(self.point[i][0] - self.point[j][0]) <= 300:  # 간격이 5분 이하이면 제거
                     del self.point[j]
                 else:
                     j += 1
-            i += 1
 
-        if len(self.point) < 3:
-            self.point = count[0:3]
-        else:
-            self.point = self.point[0:3]
+        self.point = count[0:3] if len(self.point) < 3 else self.point[0:3]
         self.point.sort(key=lambda ele: ele[0])
         self.print_point_hhmmss()
-
 
     def print_point_hhmmss(self):
         for i in range(len(self.point)):
             print(datetime.timedelta(seconds=self.point[i][0]), self.point[i][1])
-
 
     def print_section_hhmmss(self):
         for i in range(len(self.section)):
